afcc0acaa4ef7c9067ba7247246616e0792f5c1e
Chirag Sharma <167512329+Chirag-sharma04@users.noreply.github.com>
2024-09-05 12:30:49 +0530

    Initial commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,91 @@
+/**
+ * Footer component
+ *
+ * Displays avenues to contact you.
+ * Contact information is passed in from the App component that
+ * renders the Footer.
+ *
+ * If a social value has an empty string it will not be displayed.
+ */
+import React from "react";
+import PropTypes from "prop-types";
+
+
+import envelopeIcon from "../images/socials/envelope.svg";
+import gitHubIcon from "../images/socials/github.svg";
+import linkedInIcon from "../images/socials/linkedin.svg";
+
+/**
+ * 💡 Learning resources
+ *
+ *  HTML hyperlinks: https://www.w3schools.com/html/html_links.asp
+ *  Opening links in new tabs: https://www.freecodecamp.org/news/how-to-use-html-to-open-link-in-new-tab/
+ */
+
+const Footer = (props) => {
+  const {
+    email,
+    gitHub,
+    linkedIn,
+    name,
+  } = props;
+
+  return (
+    <div
+      id="footer"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "2.5rem",
+        padding: "5rem 0 3rem",
+        backgroundColor: primaryColor,
+        width: "100vw"
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "2.5rem",
+        }}
+      >
+        {email && (
+          <a href={`mailto:${email}`}>
+            <img src={envelopeIcon} alt="email" className="socialIcon" />
+          </a>
+        )}
+        {gitHub && (
+          <a href={`https://github.com/${gitHub}`} target="_blank" rel="noopener noreferrer">
+            <img src={gitHubIcon} alt="GitHub" className="socialIcon" />
+          </a>
+        )}
+        {linkedIn && (
+          <a
+            href={`https://www.linkedin.com/in/${linkedIn}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedInIcon} alt="LinkedIn" className="socialIcon" />
+          </a>
+        )}
+      </div>
+      <p className="small" style={{ marginTop: 0, color: "white" }}>
+        Created by {name}
+      </p>
+    </div>
+  );
+};
+
+Footer.defaultProps = {
+  name: "",
+};
+
+Footer.propTypes = {
+  email: "c.sharma4002@gmail.com",
+  gitHub: "https://github.com/Chirag-sharma04",
+  linkedIn: "www.linkedin.com/in/chirag-sharma-4b05b22a9",
+  name:"Chirag Sharma",
+};
+
+export default Footer;
---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,80 @@
+/**
+ * Portfolio component
+ *
+ * Highlights some of  your creations. These can be designs, websites,
+ * open source contributions, articles you've written and more.
+ *
+ * This is a great area for you to to continually add to and refine
+ * as you continue to learn and create.
+ */
+
+import React from "react";
+
+/**
+ * Desk image
+ *
+ * Below is a sample desk image. Feel free to update this to an image of your choice,
+ * updating below imageAltText to string that represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a photo you
+ * freely use on your site.
+ */
+import image from "../images/design-desk.jpeg";
+
+const imageAltText = "desktop with books and laptop";
+
+/**
+ * Project list
+ *
+ * An array of objects that will be used to display for your project
+ * links section. Below is a sample, update to reflect links you'd like to highlight.
+ */
+const projectList = [
+  {
+    title: "Amazon Clone",
+    description:
+      "I created a Amazon Clone using HTML and CSS. The project will mimic Amazon's homepage layout, featuring prominent banners, product categories, and featured products.",
+    url: "https://chirag-sharma04.github.io/Amazon-clone/",
+  },
+  {
+    title: "Stone-Paper-Scissors",
+    description:
+      " This project showcases my proficiency in front-end web development and my ability to create interactive and engaging user experiences.",
+    url: "https://chirag-sharma04.github.io/Stone-paper-scissor/",
+  },
+  {
+    title: "Currency Converter",
+    description:
+      " This application leverages HTML for the structure, CSS for styling, and JavaScript for the dynamic functionality, including fetching real-time exchange rates from an external API.",
+    url: "https://chirag-sharma04.github.io/Currency_convertor/",
+  },
+];
+
+const Portfolio = () => {
+  return (
+    <section className="padding" id="portfolio">
+      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <img
+            src={image}
+            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            alt={imageAltText}
+          />
+        </div>
+        <div className="container">
+          {projectList.map((project) => (
+            <div className="box" key={project.title}>
+              <a href={project.url} target="_blank" rel="noopener noreferrer">
+                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+              </a>
+              <p className="small">{project.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
